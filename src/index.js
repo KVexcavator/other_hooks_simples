9d05d84c48ea1205a94e4c000f9814cd4224a6f3
Excavanor <str_token@example.com>
2021-12-24 13:47:47 +0300

    start impl useResucer

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import Checkbox from './intro-use-effect/Checkbox';
 // import Phrase from "./intro-use-effect/Phrase";
-import Checkbox from './intro-use-reducer/Checkbox';
+// import Checkbox from './intro-use-reducer/Checkbox';
+import User from './intro-use-reducer/User';
 
 ReactDOM.render(
   // <Checkbox />
   // <Phrase />
-  <Checkbox />,
+  // <Checkbox />
+  <User />,
   document.getElementById('root')
 );
---
+++ src/intro-use-reducer/User.js
@@ -0,0 +1,9 @@
+import {useReduser} from "react";
+
+function User(){
+  return(
+    <h1>useReducer</h1>
+  )
+}
+
+export default User;
