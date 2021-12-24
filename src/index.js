7e9262d9e69f52447f29ad91d770f529ec68b25d
Excavanor <str_token@example.com>
2021-12-24 15:22:18 +0300

    start impl productivity encrease

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 // import Checkbox from './intro-use-effect/Checkbox';
 // import Phrase from "./intro-use-effect/Phrase";
 // import Checkbox from './intro-use-reducer/Checkbox';
-import User from './intro-use-reducer/User';
+// import User from './intro-use-reducer/User';
+import App from './update-use-hooks/App';
 
 ReactDOM.render(
   // <Checkbox />
   // <Phrase />
   // <Checkbox />
-  <User />,
+  // <User />
+  <App />,
   document.getElementById('root')
 );
---
+++ src/update-use-hooks/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+const Cat = ({ name }) => {
+  console.log(`rendering ${name}`);
+  return <p>{name}</p>;
+};
+
+function App() {
+  const [cats, setCats] = useState(["Biscuit", "Jungle", "Outlaw"]);
+  return (
+    <>
+      {cats.map((name, i) => (
+        <Cat key={i} name={name} />
+      ))}
+      <button onClick={() => setCats([...cats, prompt("Name a cat")])}>
+        Add a Cat
+      </button>
+    </>
+  );
+}
+
+export default App;
