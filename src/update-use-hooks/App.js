f80677d23498bb9fa689bdb46623ac89fc4ddc3b
Excavanor <str_token@example.com>
2021-12-24 15:57:00 +0300

    use memo to refactoring

--- src/update-use-hooks/App.js
+++ src/update-use-hooks/App.js
@@ -1,16 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, memo } from "react";
 
-const Cat = ({ name }) => {
+const Cat = ({ name, meow = f => f }) => {
   console.log(`rendering ${name}`);
-  return <p>{name}</p>;
+  return <p onClick={() => meow(name)}>{name}</p>;
 };
+// memo отображаться только при изменении его свойств
+// без него прикаждом говом добавлении перерендятся все записи
+// Второй аргумент— предикат. Предикат — функция,возвращает true/false.
+// memo(Cat, () => true) будет отображаться один раз, а затем никогда
+// memo(Cat, () => false) будет обображаться всегда
+// Только когда предикат возвращает false , Cat отображается заново.
+const PureCat = memo(Cat, (prevProps, nextProps) => prevProps.name === nextProps.name);
 
 function App() {
   const [cats, setCats] = useState(["Biscuit", "Jungle", "Outlaw"]);
   return (
     <>
       {cats.map((name, i) => (
-        <Cat key={i} name={name} />
+        <PureCat key={i} name={name} meow={name => console.log(`${name} has meowed`)} />
       ))}
       <button onClick={() => setCats([...cats, prompt("Name a cat")])}>
         Add a Cat
