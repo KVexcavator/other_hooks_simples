10665cc28ddc1bd745393b4eb3ece236454e16df
Excavanor <str_token@example.com>
2021-12-24 13:15:26 +0300

    start intro the useReducer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import Checkbox from './intro-use-effect/Checkbox';
-import Phrase from "./intro-use-effect/Phrase";
+// import Phrase from "./intro-use-effect/Phrase";
+import Checkbox from './intro-use-reducer/Checkbox';
 
 ReactDOM.render(
   // <Checkbox />
-  <Phrase />,
+  // <Phrase />
+  <Checkbox />,
   document.getElementById('root')
 );
---
+++ src/intro-use-reducer/Checkbox.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+function Checkbox() {
+  const [checked, setChecked] = useState(false);
+ 
+  return (
+    <>
+      <input
+        type="checkbox"
+        value={checked}
+        onChange={() => setChecked(checked => !checked)}
+      />
+      {checked ? "checked" : "not checked"}
+      <hr />
+    </>
+  );   
+}
+
+export default Checkbox;
