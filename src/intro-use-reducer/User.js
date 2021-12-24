ee001194982f9742cb65b6aa6083affe1e97123a
Excavanor <str_token@example.com>
2021-12-24 14:51:56 +0300

    impl by useState

--- src/intro-use-reducer/User.js
+++ src/intro-use-reducer/User.js
@@ -1,8 +1,33 @@
-import {useReduser} from "react";
+import {useReduser, useState} from "react";
+
+const firstUser = {
+  id: "0391-3233-3201",
+  firstName: "Bill",
+  lastName: "Wilson",
+  city: "Missoula",
+  state: "Montana",
+  email: "bwilson@mtnwilsons.com",
+  admin: false
+};
 
 function User(){
+  const [user, setUser] = useState(firstUser);
+
   return(
-    <h1>useReducer</h1>
+    <div>
+      <h1>
+        {user.firstName} {user.lastName} — {user.admin ? "Admin" : "User"}
+      </h1>
+      <p>Email: {user.email}</p>
+      <p>
+        Location: {user.city}, {user.state}
+      </p>
+      <button
+        onClick={() => {
+          setUser({ ...user, admin: true });
+        }}
+      >Make Admin</button>
+    </div>
   )
 }
 
