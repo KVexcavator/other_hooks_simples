d0bd8038732eb4dc769b9d9a8d7e6ca5b723c51c
Excavanor <str_token@example.com>
2021-12-29 13:50:53 +0300

    add two layers fetch to logic abctaction

--- src/deep-in-fetch/GitHubUser.js
+++ src/deep-in-fetch/GitHubUser.js
@@ -1,3 +1,31 @@
-export default function GitHubUser({login}){
- return <h1>{login}</h1>
+import Fetch from "./Fetch";
+
+export default function GitHubUser({ login }) {
+  return (
+    <Fetch
+      uri={`https://api.github.com/users/${login}`}
+      renderError={error => {
+        // handle error
+        return <p>Something went wrong... {error.message}</p>;
+      }}
+      renderSuccess={UserDetails}
+    />
+  );
+}
+
+function UserDetails({ data }) {
+  return (
+    <div>
+      <img
+        src={data.avatar_url}
+        alt={data.login}
+        style={{ width: 200 }}
+      />
+      <div>
+        <h1>{data.login}</h1>
+        {data.name && <p>{data.name}</p>}
+        {data.location && <p>{data.location}</p>}
+      </div>
+    </div>
+  );
 }
