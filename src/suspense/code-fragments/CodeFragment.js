b6390cb9557e8b187fa9d2cd563975e887a013f3
Excavanor <str_token@example.com>
2022-01-06 14:34:19 +0300

    impl suspense code load

--- src/suspense/code-fragments/CodeFragment.js
+++ src/suspense/code-fragments/CodeFragment.js
@@ -1,7 +1,24 @@
-import '../style.css'
+import React, { useState, Suspense, lazy } from 'react';
+import Agreement from './Agreement';
+// React Spinners — это библиотека анимированных спиннеров загрузки
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+import ErrorBoundary from '../error-boundary/ErrorBoundary';
+import '../style.css';
+
+// отложить загрузку компонента Main до его рендеринга
+const Main = React.lazy(() => import("./Main"));
 
 export default function CodeFragment(){
-  return (
-    <h1>i code fragment</h1>
-  )
+  const [agree, setAgree] = useState(false)
+
+  if (!agree)
+    return <Agreement onAgree={() => setAgree(true)} />
+
+    return (
+      <ErrorBoundary>
+        <Suspense fallback={<ClimbingBoxLoader />}>
+          <Main />
+        </Suspense>
+      </ErrorBoundary>
+    );
 }
---
+++ src/suspense/code-fragments/Main.js
@@ -0,0 +1,35 @@
+import ErrorBoundary from "./ErrorBoundary";
+import '../style.css';
+
+const SiteLayout = ({ children, menu = c => null }) => {
+  return (
+    <div className="site-container">
+      <div className='first'>{menu}</div>
+      <div className='second'>{children}</div>
+    </div>
+  );
+};
+
+const Menu = () => (
+  <ErrorBoundary>
+    <p style={{ color: "white" }}>TODO: Build Menu</p>
+  </ErrorBoundary>
+);
+
+const Callout = ({children}) => (
+  <ErrorBoundary>
+    <div className='callout'>{children}</div>
+  </ErrorBoundary>  
+)
+
+export default function Main() {
+  return (
+    <SiteLayout menu={<Menu />}>
+      <Callout>Welcome to the site</Callout>
+      <ErrorBoundary>
+        <h1>TODO: Home Page</h1>
+        <p>Complete the main contents for this home page</p>
+      </ErrorBoundary>
+    </SiteLayout>
+  );
+}
